734dddbe9af11a8069b65c622396d6b69fee7410
Dimcheck <dfarinyak@gmail.com>
2022-09-30 00:20:42 +0300

    db connection on start with working routes

--- demo_app/main.py
+++ demo_app/main.py
@@ -2,9 +2,11 @@ from aiohttp import web
 
 from settings import config
 from routes import setup_routes
+from db import pg_context
 
 
 app = web.Application()
-setup_routes(app)
 app['config'] = config
+app.cleanup_ctx.append(pg_context)
+setup_routes(app)
 web.run_app(app)
