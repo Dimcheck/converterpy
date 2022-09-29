69efc6c20836e9a0e7352addb0f3ba14bffff5ca
Dimcheck <dfarinyak@gmail.com>
2022-09-30 00:21:20 +0300

    db output

--- demo_app/views.py
+++ demo_app/views.py
@@ -1,5 +1,11 @@
 from aiohttp import web
+from models import question, choice
 
 
 async def index(request):
-    return web.Response(text='Slava Ukaraini')
+    async with request.app['db'].acquire() as conn:
+        cursor = await conn.execute(choice.select())
+        records = await cursor.fetchall()
+        data = [dict(q) for q in records]
+        debug = [print(dict(q)) for q in records]
+        return web.Response(text=str(data))
